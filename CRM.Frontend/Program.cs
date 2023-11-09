fa1b6b03721baa56b364ff508f74c90dc45ebec7
Sara Melissa Marroquín Vega <sara.marroquin@inducascos.com>
2023-11-09 15:54:36 -0500

    Security Frontend

--- CRM.Frontend/Program.cs
+++ CRM.Frontend/Program.cs
@@ -1,11 +1,15 @@
 using CRM.Frontend;
+using CRM.Frontend.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7152/") });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 await builder.Build().RunAsync();
